9b0abd919bc08670d7891640f29b948b4bb86cb3
xt9 <niklas_andersson90@hotmail.com>
2019-11-10 16:21:21 +0100

    Removed unused dependency, also testing out VSC git integration

--- src/components/context/NotificationContext.js
+++ src/components/context/NotificationContext.js
@@ -1,4 +1,4 @@
-import React, { useReducer, useEffect } from 'react';
+import React, { useReducer } from 'react';
 import { reducer } from '../../hooks/useNotifications';
 
 const NotificationContext = React.createContext();
